6fa3eaf304b9a55d39e84934ba5cbd2769d475f5
Patrik <Almaszorp96>
2018-04-25 20:35:26 +0200

    Home page, index.xhtml

--- RecruitmentApp/src/com/recruitment/web/RatingController.java
+++ RecruitmentApp/src/com/recruitment/web/RatingController.java
@@ -6,6 +6,7 @@ import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
+import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
